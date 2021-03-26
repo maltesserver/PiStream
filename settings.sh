3d67dee6dba177148bdaca786611f3ead43a6834
Malte <56297305+maltesserver@users.noreply.github.com>
2021-03-26 13:02:47 +0000

    User Input
    Added user input for ease of access

--- settings.sh
+++ settings.sh
@@ -1,1 +1,4 @@
-sudo docker run --privileged --name cam -ti alexellis2/streaming:latest xxxx-xxxx-xxxx-xxxx
+key="xxxx-xxxx-xxxx-xxxx"
+read -p "$key" -p "Please enter your YouTube Streamkey: " input
+key="${input:-$key}"
+sudo docker run --privileged --name cam -ti alexellis2/streaming:latest $key
